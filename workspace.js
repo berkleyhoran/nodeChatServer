cba025931d8ccdb210903ceca292d31047c6b9ce
Berkley Horan <horans11@gmail.com>
2019-03-13 19:41:57 -0600

    finished chat loggin and chat

--- workspace.js
+++ workspace.js
@@ -1,4 +1,5 @@
 const net = require('net');
+const fs = require('fs');
 var users = [];
 
 
@@ -9,6 +10,7 @@ let server = net.createServer(client => {
     users.push(client);
     client.setEncoding('utf8')
     console.log(client.name + ' has joined');
+    fs.appendFile('./chat.log', client.name + ' has joined\n', (error) => {if (error) throw error});
     for(let i = 0; i < users.length; i++) {
         if(client.name != users[i].name){
             users[i].write(client.name + ' has joined');   
@@ -18,17 +20,19 @@ let server = net.createServer(client => {
         }
     }
     client.on('data', data => {
+        fs.appendFile('./chat.log', client.name + ': ' + data, (error) => {if (error) throw error});
+        console.log(client.name + ': ' + data);
         for(let i = 0; i < users.length; i++) {
             if(client.name != users[i].name){
-                users[i].write(users[i].name + ': ' + data);
+                users[i].write(client.name + ': ' + data);
             }
-            console.log(users[i].name + ': ' + data);
         }
     })
     client.on('close', function() {
+        fs.appendFile('./chat.log', client.name + ' has left\n', (error) => {if (error) throw error});
         console.log(client.name + ' left the chat');
             for (let x = 0; x < users.length; x++) {
-                if(client.name === users[x]){
+                if(client.name == users[x]){
                     users.splice(x, 1);
                 } 
             }
@@ -39,3 +43,4 @@ let server = net.createServer(client => {
 // process.stdin.pipe(client);
 
 console.log('Listening on Port 5000')
+fs.appendFile('./chat.log', 'Server started\n', (error) => {if (error) throw error});
